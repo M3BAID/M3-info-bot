12ecc0a2493daf578e32772f986b03163e97e850
Mohammed <51389957+M3BAID@users.noreply.github.com>
2023-05-10 14:13:33 +0000

    Edit

--- bot.ts
+++ bot.ts
@@ -1,24 +1,27 @@
 import "https://deno.land/std@0.186.0/dotenv/load.ts";
 import { Bot } from "https://deno.land/x/grammy@v1.16.0/mod.ts";
-import { Api } from "https://deno.land/x/grammy@v1.16.0/mod.ts";
 
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN")
 
+if (typeof BOT_TOKEN === "undefined")
+  throw new Error("BOT_TOKEN is not defined")
 const bot = new Bot(BOT_TOKEN); 
 
-//await bot.api.sendMessage(xxxxxxx,"Hi!");
-// console.log(message.message_id);
-
-bot.hears(".", async (ctx) => {
-  await ctx.reply("test", {
+bot.command("test", async (ctx) => {
+  await ctx.reply("Everything is Alright!", {
     reply_to_message_id: ctx.msg.message_id,
   });
 });
 
-bot.command("start", async (ctx) => {
-  await ctx.reply("User is: @" + ctx.chat.username + " " + "id: " + ctx.chat.id + " " + ctx.msg.message_id );
-  console.log(ctx.chat.username + " , " + ctx.chat.id + " , " + ctx.chat.message_id);
+bot.command("info", async (ctx) => {
+  await ctx.reply(`
+First name: ${ctx.from?.first_name || ""}
+User is: @${ctx.from?.username || ""} 
+id: ${ctx.chat.id}
+Language code: ${ctx.from?.language_code || ""}
+Bot?: ${ctx.from?.is_bot}`);
+console.log(`UserName: ${ctx.from?.username} ,id: ${ctx.chat.id}`);
 });
 
 console.log("bot started");
